afcfbc47470fb165a9ea13ac76e485c7892475d8
Layla_Alshehri <layla.jaber.m@gmail.com>
2025-12-24 14:36:32 +0300

    add prototype to header

--- shell.h
+++ shell.h
@@ -4,38 +4,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <signal.h>
 #include <sys/wait.h>
 #include <sys/stat.h>
 #include <string.h>
 #include <unistd.h>
 
-
-/**
- * prompt - displays the shell prompt
- */
-void prompt(void);
-
-/**
- * read_line - reads input from stdin
- * Return: pointer to input string
- */
-char *read_line(void);
-
-/**
- * find_path - finds full path of a command using PATH
- * @command: command name
- * Return: full path or NULL
- */
-char *find_path(char *command);
-
-/**
- * execute - executes a command
- * @command: command to execute
- * Return: 1 on success
- */
-int execute(char *command);
-
 extern char **environ;
-char *parse(char *line);
 
-#endif /* SHELL_H */
+void display_prompt(void);
+char *read_line(void);
+void execute_command(char *command);
+
+#endif
